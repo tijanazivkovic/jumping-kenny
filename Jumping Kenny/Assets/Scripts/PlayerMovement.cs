f39fec8ebd47e59b5c27533f8396880aee734c25
Isidora Slavkovic <isidora.slavkovic@gmail.com>
2022-04-21 11:08:05 +0200

    Added jump and turns.

--- Jumping Kenny/Assets/Scripts/PlayerMovement.cs
+++ Jumping Kenny/Assets/Scripts/PlayerMovement.cs
@@ -20,9 +20,36 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonUp("Walk")) {
             player.GetComponent<Animator>().Play("HumanoidIdle");
-
         }
 
+        if (Input.GetButtonDown("Jump")) {
+            player.GetComponent<Animator>().Play("HumanoidJumpUp");
+        }
+
+        if (Input.GetButtonDown("Left")) {
+            player.GetComponent<Animator>().Play("StandQuarterTurnLeft");
+        }
+
+        if (Input.GetButtonUp("Left")) {
+            player.GetComponent<Animator>().Play("HumanoidIdle");
+        }
+
+        if (Input.GetButtonDown("Right")) {
+            player.GetComponent<Animator>().Play("StandQuarterTurnRight");
+        }
+
+        if (Input.GetButtonUp("Right")) {
+            player.GetComponent<Animator>().Play("HumanoidIdle");
+        }
+
+        if (Input.GetButtonDown("Down")) {
+            player.GetComponent<Animator>().Play("StandHalfTurnRight");
+        }
+
+        if (Input.GetButtonUp("Down")) {
+            player.GetComponent<Animator>().Play("HumanoidIdle");
+        }
+        
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         Vector3 changeInPosition = new Vector3(horizontal, 0f, vertical);
